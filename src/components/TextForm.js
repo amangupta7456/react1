314ba0dce8c10849e56b1acdb0cc6244565997d9
amangupta7456 <amangupta7456.ag@gmail.com>
2025-01-05 23:24:42 +0530

    doing dark theme of navbar nd creating count words nd charcter with preview

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,17 +7,32 @@ export default function TextForm(props) {
         let newText = Text.toUpperCase();
         setText(newText);
     };
+    const handleLoClick = ()=>{
+      let newText = Text.toLowerCase();
+      setText(newText);
+    };
     const handleOnChange = (event)=>{
         setText(event.target.value);
     };
-    const[Text, setText] = useState("Enter text here");
+    const[Text, setText] = useState("");
   return (
-    <div>
+  <>
+    <div className="container">
         <h1>{props.heading}</h1>
         <div   className="mb-3">
         <textarea   className="form-control" value={Text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
+
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to LowerCase</button>
     </div>
+    <div className="container">
+        <h2>Your text summary</h2>
+        <p>{Text.split(" ").length} words and {Text.length} characters</p>
+        <p>{0.008 * Text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{Text}</p>
+    </div>
+  </>
   );
 }
