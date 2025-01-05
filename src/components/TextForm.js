8521b498e645a002a57c6837df9ee18aebedec6b
amangupta7456 <amangupta7456.ag@gmail.com>
2025-01-05 21:47:18 +0530

    correcting heading

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -13,7 +13,7 @@ export default function TextForm(props) {
     const[Text, setText] = useState("Enter text here");
   return (
     <div>
-        <h1>{props.heading} - {Text} </h1>
+        <h1>{props.heading}</h1>
         <div   className="mb-3">
         <textarea   className="form-control" value={Text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
